5c03515f31a1d226958f2dff504f27f7d89bd653
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-09 17:17:36 +0800

    manssh version v0.1.0

---
+++ manssh.rb
@@ -0,0 +1,16 @@
+class Manssh < Formula
+  desc "Manage your ssh alias configs easily"
+  homepage "https://github.com/xwjdsh/manssh"
+  url "https://github.com/xwjdsh/manssh/releases/download/v0.1.0/manssh_0.1.0_darwin_amd64.tar.gz"
+  version "0.1.0"
+  sha256 "61a8fab498e3cf0fa14877fa8326faea558ee871b0450996e00161a385c08f98"
+
+  def install
+    bin.install "manssh"
+    ...
+  end
+
+  test do
+    
+  end
+end
