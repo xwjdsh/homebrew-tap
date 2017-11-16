81185a8f6d5753306f14faafe39b1369520d0d19
Wendell Sun <iwendellsun@gmail.com>
2017-11-16 02:06:03 -0600

    manssh version v0.3.0

--- manssh.rb
+++ manssh.rb
@@ -3,7 +3,7 @@ class Manssh < Formula
   homepage "https://github.com/xwjdsh/manssh"
   url "https://github.com/xwjdsh/manssh/releases/download/v0.3.0/manssh_0.3.0_darwin_amd64.tar.gz"
   version "0.3.0"
-  sha256 "f9ebfc48bc6e87293b44716dd87eaf1bc14319c56ec5c2c590d702f1bae873ca"
+  sha256 "1b6c227086839f8ec3fb1c26b1db04fbfe82d95678e64421c7c7971459f0490c"
 
   def install
     bin.install "manssh"
