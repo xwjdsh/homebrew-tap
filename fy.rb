59d23e5795a9d1d82e1663da10595481f71f0aae
Wendell Sun <iwendellsun@gmail.com>
2018-04-30 02:54:17 +0800

    fy version v0.2.0

--- fy.rb
+++ fy.rb
@@ -1,9 +1,9 @@
 class Fy < Formula
   desc "A command-line translation tool"
   homepage "https://github.com/xwjdsh/fy"
-  url "https://github.com/xwjdsh/fy/releases/download/v0.1.0/fy_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "78aa80f1b550513c72d55bacfded81be43228764d54de39cf9ef0bdf114056f0"
+  url "https://github.com/xwjdsh/fy/releases/download/v0.2.0/fy_0.2.0_darwin_amd64.tar.gz"
+  version "0.2.0"
+  sha256 "1cc2fd03c50addc9f2e3edacb14a21f57a0692975ff8a3f6936aae80395fd129"
 
   def install
     bin.install "fy"
