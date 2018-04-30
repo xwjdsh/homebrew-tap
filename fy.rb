2c89202a123f36d607abb6584fc26544c09ef96c
Wendell Sun <iwendellsun@gmail.com>
2018-04-30 13:32:47 +0800

    fy version v0.3.0

--- fy.rb
+++ fy.rb
@@ -3,7 +3,7 @@ class Fy < Formula
   homepage "https://github.com/xwjdsh/fy"
   url "https://github.com/xwjdsh/fy/releases/download/v0.3.0/fy_0.3.0_darwin_amd64.tar.gz"
   version "0.3.0"
-  sha256 "7e5759773ef19267b74fc2a9749ff8a7169f02ddc2dc62f22e73ed03a38e24c6"
+  sha256 "d7293457f3964b6f938887acc3e0f5f6773c37702244ff08a4f06977d1205932"
 
   def install
     bin.install "fy"
